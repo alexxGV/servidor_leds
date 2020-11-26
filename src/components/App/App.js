53bf6587c43b0b30fda465d63b490a0ce41c3f92
Raspberry Alex <alexx.garci@gmail.com>
2020-11-26 10:32:34 +0100

    Primer commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { SketchPicker } from 'react-color';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <SketchPicker />
+      </div>
+    )
+  }
+}
